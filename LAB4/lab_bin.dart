7d0f31d84e06f5df78da20e770b069de1d8aeced
Abhilash3045 <dopatiabhilash@gmail.com>
2025-03-14 20:52:11 +0530

    Delete LAB4/lab_bin.dart

--- LAB4/lab_bin.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lab4/fruit_sdemo.dart';
-import 'dart:io';
-void main(List<String> arguments) {
-	switch_demo sd = switch_demo();
-	print("enter a fruit name: ");
-	String fruit = stdin.readLineSync()!;
-	print(sd.scase(fruit));
-}
