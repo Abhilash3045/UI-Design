5669d00b2e4ae5635be8a5feef0f1adb6bdc43d0
Abhilash3045 <dopatiabhilash@gmail.com>
2025-06-05 11:11:10 +0530

    Update main.dart

--- LAB9/main.dart
+++ LAB9/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+// Import your widget files
+import 'desktop.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Media Query',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Desktop(),
+    );
+  }
+}
